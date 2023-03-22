9a7c69fcf4f13477c90378b4232db976b52dabf0
namydhc1 <92814689+namydhc1@users.noreply.github.com>
2023-03-22 15:00:50 +0700

    h

--- src/java/Controller/att/lecturer/TakeAttendanceController.java
+++ src/java/Controller/att/lecturer/TakeAttendanceController.java
@@ -16,22 +16,10 @@ import java.util.ArrayList;
 import model.att.Student;
 import model.att.Attendance;
 
-/**
- *
- * @author sonnt
- */
 public class TakeAttendanceController extends HttpServlet {
    
     
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
@@ -41,13 +29,6 @@ public class TakeAttendanceController extends HttpServlet {
         request.getRequestDispatcher("../view/att/lecturer/att.jsp").forward(request, response);
     } 
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
@@ -68,10 +49,7 @@ public class TakeAttendanceController extends HttpServlet {
         response.sendRedirect("takeattend?id="+sessionid);
     }
 
-    /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
+
     @Override
     public String getServletInfo() {
         return "Short description";
